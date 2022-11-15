7a192fe91898d059ce9d5311f41f6fbd41b29df7
Dilip <tagyourtaxi@gmail.com>
2022-11-15 13:10:32 +0530

    new files added

---
+++ app/Http/Requests/Admin/Faq/CreateFaqRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Admin\Faq;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateFaqRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'service_location_id' => 'required',
+            'user_type' => 'required',
+            'question' => 'required',
+            'answer' => 'required',
+            'transport_type' =>'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'service_location_id.required' => 'Area name is required'
+        ];
+    }
+}
